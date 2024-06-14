5f9fe55419d76288bd251309fc9480a832825294
KDr2 <zhuo.dev@gmail.com>
2024-06-15 06:35:00 +0800

    add license info

--- info.rkt
+++ info.rkt
@@ -4,4 +4,5 @@
 ;; (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Kittle-Buffer, an extended Brainf**k implementation.")
 (define version "0.2.1")
-(define pkg-authors '(KDr2))
+(define pkg-authors '(Killian))
+(define license '(Apache-2.0 OR MIT))
